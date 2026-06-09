22d2f41b07b6efb8d16e46fb90b00c4cdac4bbc4
OKLMARO <mounioma@gmail.com>
2026-06-09 13:41:33 +0200

    def func

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: oamairi <oamairi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/26 12:21:29 by oamairi           #+#    #+#             */
-/*   Updated: 2026/03/26 12:21:59 by oamairi          ###   ########.fr       */
+/*   Updated: 2026/06/09 13:40:40 by oamairi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,26 @@
 
 Bureaucrat::Bureaucrat(){};
 
+Bureaucrat::Bureaucrat(unsigned int grade) : _grade(grade) {};
+
+void	Bureaucrat::upGrade()
+{
+	
+}
+
+const std::string	Bureaucrat::getName()
+{
+	return this->_name;
+}
+
+int	Bureaucrat::getGrade()
+{
+	return this->_grade;
+}
+
+void	Bureaucrat::downGrade()
+{
+	
+}
+	
 Bureaucrat::~Bureaucrat(){};
